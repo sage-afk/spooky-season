81ea7b401512d85e4f6b42578d7b08a3f6119b35
Broderick, Sage <sage.broderick@erdc.dren.mil>
2025-10-03 17:24:46 -0500

    test

--- spooky-season/src/router/index.ts
+++ spooky-season/src/router/index.ts
@@ -9,7 +9,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL + '/spooky-season.github.io/'),
   routes,
 })
 
